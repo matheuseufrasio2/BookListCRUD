7ac1b8ae2f935c08b9c21ee437a8df4afd8e475e
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2019-11-02 15:31:47 -0300

    CRUD terminado

---
+++ BookListMVC/Controllers/BooksController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookListMVC.Controllers
+{
+    public class BooksController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public BooksController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Books.ToList());
+        }
+
+        //GET Book/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(book);
+                await _db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+
+        //GET EDIT BOOK
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //EDIT POST
+        [HttpPost]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> Edit(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                var BookFromDb = await _db.Books.FirstOrDefaultAsync(b => b.Id == book.Id);
+                BookFromDb.Name = book.Name;
+                BookFromDb.Author = book.Author;
+                BookFromDb.Price = book.Price;
+
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(book);
+        }
+
+        //GET DELETE BOOK
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+            return View(book);
+        }
+
+        //DELETE POST
+        [HttpPost, ActionName("Delete")]
+        [AutoValidateAntiforgeryToken]
+        public async Task<IActionResult> RemoveBook(int? id)
+        {
+            var book = await _db.Books.SingleOrDefaultAsync(m => m.Id == id);
+            _db.Books.Remove(book);
+
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ BookListMVC/Migrations/20191102153511_updatePriceofBooktoDouble.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListMVC.Migrations
+{
+    public partial class updatePriceofBooktoDouble : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(double));
+        }
+    }
+}
